932f49bb454532760a61b1cea89a91dfa17c7366
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-08-22 10:58:54 +0200

    ändrat testMain.py så att det följer vår kodningsstandard, även optimerat lite

--- tests/testMain.py
+++ tests/testMain.py
@@ -5,51 +5,49 @@ from os import path, getcwd
 
 
 
-class TestHemsida(TestCase):
+class TestWebsite(TestCase):
 
-    # inställningar för hur testerna körs
-    stangintebrowsern = True  # om True så hålls webbläsaren öppen efter testerna är klara, annars stängs den
-    gomfonstret = True  # visar webbläsaren medan testerna körs
+    # settings for how the tests will be executed
+    do_not_close_browser = True  # if True, the browser will stay open after the tests are done, otherwise it will close
+    hide_window = False  # if True, the browser will not be shown while the tests are executed
 
-    # setUpClass körs INNAN FÖRSTA testet
+    # setUpClass is executed BEFORE THE FIRST test
     @classmethod
     def setUpClass(cls):
         chr_options = Options()
 
-        if cls.stangintebrowsern:
+        if cls.do_not_close_browser:
             chr_options.add_experimental_option("detach", True)
 
-        if cls.gomfonstret:
+        if cls.hide_window:
             chr_options.add_argument("--headless")
 
+        chr_options.add_argument("--disable-search-engine-choice-screen")
         cls.browser = webdriver.Chrome(options=chr_options)
 
-    # tearDownClass körs EFTER SISTA testet
+    # tearDownClass is executed AFTER THE LAST test
     @classmethod
     def tearDownClass(cls):
-        pass  # gör ingenting
+        pass
 
-    # setUp körs INNAN VARJE TEST
+    # setUp is executed BEFORE EVERY TEST
     def setUp(self):
-        pass  # gör ingenting
-
-    # tearDown körs EFTER VARJE TEST
-    def tearDown(self):
-        self.browser.get('about:blank')  # gå till en tom sida för att undvika att tidigare test påverkar senare
-
-#ändrinf
-    # HÄR BÖRJAR TESTERNA
-    def testPageText(self):
         self.browser.get(path.join(path.dirname(getcwd()), "JITS-pizzeria", 'index.html'))
+
+    # tearDown is executed AFTER EVERY TEST
+    def tearDown(self):
+        self.browser.get('about:blank')  # load a blank page to avoid previous tests affecting subsequent tests
+
+    # THE TESTS START HERE
+    def testPageText(self):
         self.assertIn("Välkommen", self.browser.page_source)
 
     def testPageNumber(self):
-        self.browser.get(path.join(path.dirname(getcwd()), "JITS-pizzeria", 'index.html'))
         self.assertIn("018-123456", self.browser.page_source)
 
 
 
 
-# denna bit finns här så att testerna körs om filen körs som vanligt python-program
+# this code is here so that the tests will be executed if the file is executed as a normal python program
 if __name__ == '__main__':
     main(verbosity=2)
