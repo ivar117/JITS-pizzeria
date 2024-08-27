2117fafb8d2de68132475ec33ebf3dbc7640e3f7
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-08-27 11:57:17 +0200

    tog bort nodemodules och package lock

--- build/main.js
+++ build/main.js
@@ -3,12 +3,12 @@ function fetch_products() {
     fetch("products.json")
         .then(resp => resp.json())
         .then(products => {
-        display_products(products);
+        display_products(products); // Display products on the page
     });
 }
 function display_products(products) {
     const left_menu_container = document.getElementById("left-menu-container");
     const right_menu_container = document.getElementById("right-menu-container");
-    console.log(products);
+    console.log(products); // Logs products in console
 }
 fetch_products();
