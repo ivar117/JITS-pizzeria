98b82d1aeb4bf7e1a3c72c8182ac3cd9594de862
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-08-30 15:12:14 +0200

    skapat en funktion som gör koden för att generera screenshots kortare

--- tests/screenshots.py
+++ tests/screenshots.py
@@ -26,28 +26,27 @@ driver = webdriver.Chrome(options=chr_options)
 driver.get(os.path.join(os.path.dirname(os.getcwd()), "JITS-pizzeria", 'index.html'))
 # -------------------------------------------------------------------------------------------------------------------------
 
+def save_screenshot(save_path, res_name, id): # function for shortening the code for saving screenshots
+    driver.save_screenshot(save_path + res_name + id  + datetime.utcnow().strftime('%Y-%m-%d %H.%M.%S.%f')[:-3] + ".png") 
+
 def screenshot_res(width, height, res_name, save_path):
-    
+
     driver.set_window_size(width, height) # set the window size to the desired resolution
 
     html = driver.find_element(By.TAG_NAME, 'html') # prepare for scroll
 
     scroll(html, "top") # scroll to top
-
     # save screenshot of the top of the page with the resolution in the filename
-    driver.save_screenshot(save_path + res_name + " top " + datetime.utcnow().strftime('%Y-%m-%d %H.%M.%S.%f')[:-3] + ".png")
+    save_screenshot(save_path, res_name, " top ")
 
     scroll(html, "bottom") # scroll to bottom
-
     # save screenshot of the bottom of the page with the resolution in the filename
-    driver.save_screenshot(save_path + res_name + " bottom " + datetime.utcnow().strftime('%Y-%m-%d %H.%M.%S.%f')[:-3] + ".png")
+    save_screenshot(save_path, res_name, " bottom ")
 
     bg_images = driver.find_elements(By.CLASS_NAME, "background") # collect all elements with the class "background" in a list
-
     for img in bg_images: # iterate over the background images and take a screenshot of each
-        scroll(html, img)
-
-        driver.save_screenshot(save_path + res_name + " img-"+ img.get_attribute("id") + " " + datetime.utcnow().strftime('%Y-%m-%d %H.%M.%S.%f')[:-3] + ".png")
+        scroll(html, img) # scroll to the image
+        save_screenshot(save_path, res_name, " img-" + img.get_attribute("id") + " ")
 
 def scroll(element, target):
     if type(target) == str:
@@ -69,15 +68,12 @@ def capture_screenshots(): # generates a screenshot of the start page in two res
         os.mkdir("generatedScreenshots")
 
     sub_folder_name = datetime.utcnow().strftime('%Y-%m-%d-%H.%M.%S.%f')[:-3] # create a subfolder-name with the current time
-
     save_path = "generatedScreenshots/" + sub_folder_name + "/" # set the save path to the subfolder
-
     os.mkdir(save_path) # create the subfolder
 
     screenshot_res(1920, 1080, "1080p", save_path) # test for checking desktop 1080p resolution
     screenshot_res(2560, 1440, "1440p", save_path) # test for checking desktop 1440p resolution
-
-    # tests for checking phone resolution
+    # tests for checking phone resolutions
     screenshot_res(375, 667, "iPhone-SE", save_path) # iPhone SE
     screenshot_res(414, 896, "iPhone-XR", save_path) # iPhone XR
     screenshot_res(390, 844, "iPhone-12-Pro", save_path) # iPhone 12 Pro
