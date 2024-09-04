1d8c563511cc889c69467df568515372d81ec73d
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-09-04 10:13:53 +0200

    det funkar nu att generera skärmdumpar i mobilupplösningar

--- tests/screenshots.py
+++ tests/screenshots.py
@@ -13,6 +13,7 @@ hide_window = True # if True, the browser will not be shown while the tests are 
 
 # ---------------------------- This section of code starts the browser and loads the website ----------------------------
 chr_options = Options()
+# chr_options = webdriver.ChromeOptions()
 
 if do_not_close_browser:
     chr_options.add_experimental_option("detach", True)
@@ -21,37 +22,35 @@ if hide_window:
     chr_options.add_argument("--headless")
 
 chr_options.add_argument("--disable-search-engine-choice-screen")
-
-driver = webdriver.Chrome(options=chr_options)
-driver.get(os.path.join(os.path.dirname(os.getcwd()), "JITS-pizzeria", 'index.html'))
 # -------------------------------------------------------------------------------------------------------------------------
 
 def get_current_date_and_time(): # function for getting the current date and time
     return datetime.now(timezone.utc).strftime('%Y-%m-%d-%H.%M.%S.%f')[:-3] # return the current date and time in a specific format
 
-def save_screenshot(save_path, res_name, id): # function for shortening the code for saving screenshots
+def save_screenshot(driver, save_path, res_name, id): # function for shortening the code for saving screenshots
     driver.save_screenshot(save_path + res_name + id + "UTC" + get_current_date_and_time() + ".png") 
 
-def screenshot_res(width, height, res_name, save_path):
+
+def screenshot_res(driver, width, height, res_name, save_path):
 
     driver.set_window_size(width, height) # set the window size to the desired resolution
 
     html = driver.find_element(By.TAG_NAME, 'html') # prepare for scroll
 
-    scroll(html, "top") # scroll to top
+    scroll(driver, html, "top") # scroll to top
     # save screenshot of the top of the page with the resolution in the filename
-    save_screenshot(save_path, res_name, " top ")
+    save_screenshot(driver, save_path, res_name, " top ")
 
-    scroll(html, "bottom") # scroll to bottom
+    scroll(driver, html, "bottom") # scroll to bottom
     # save screenshot of the bottom of the page with the resolution in the filename
-    save_screenshot(save_path, res_name, " bottom ")
+    save_screenshot(driver, save_path, res_name, " bottom ")
 
     bg_images = driver.find_elements(By.CLASS_NAME, "background") # collect all elements with the class "background" in a list
     for img in bg_images: # iterate over the background images and take a screenshot of each
-        scroll(html, img) # scroll to the image
-        save_screenshot(save_path, res_name, " img-" + img.get_attribute("id") + " ")
+        scroll(driver, html, img) # scroll to the image
+        save_screenshot(driver, save_path, res_name, " img-" + img.get_attribute("id") + " ")
 
-def scroll(element, target):
+def scroll(driver, element, target):
     if type(target) == str:
         if target == "top":
             element.send_keys(Keys.HOME) # scroll to top
@@ -65,8 +64,50 @@ def scroll(element, target):
         actions = ActionChains(driver)
         actions.move_to_element(target).perform()
 
-def capture_screenshots(): # generates a screenshot of the start page in two resolutions
+def capture_desktop_screenshots(save_path): # generates a screenshot of the start page in two resolutions
 
+    driver = webdriver.Chrome(options=chr_options) # start the browser with the options
+    driver.get(os.path.join(os.path.dirname(os.getcwd()), "JITS-pizzeria", 'index.html')) # load the website
+
+    screenshot_res(driver, 1920, 1080, "1080p", save_path) # test for checking desktop 1080p resolution
+    screenshot_res(driver, 2560, 1440, "1440p", save_path) # test for checking desktop 1440p resolution
+
+    driver.quit() # close the browser
+
+def screenshot_mobile_res(res_name, save_path):
+
+    mobile_emulation = { "deviceName": res_name } # set the device name to the desired resolution
+    chr_options.add_experimental_option("mobileEmulation", mobile_emulation) # set the mobile emulation option to the desired resolution
+
+    driver = webdriver.Chrome(options=chr_options) # start the browser with the options
+    driver.get(os.path.join(os.path.dirname(os.getcwd()), "JITS-pizzeria", 'index.html')) # load the website
+
+    html = driver.find_element(By.TAG_NAME, 'html') # prepare for scroll
+
+    scroll(driver, html, "top") # scroll to top
+    # save screenshot of the top of the page with the resolution in the filename
+    save_screenshot(driver, save_path, res_name, " top ")
+
+    scroll(driver, html, "bottom") # scroll to bottom
+    # save screenshot of the bottom of the page with the resolution in the filename
+    save_screenshot(driver, save_path, res_name, " bottom ")
+
+    bg_images = driver.find_elements(By.CLASS_NAME, "background") # collect all elements with the class "background" in a list
+    for img in bg_images: # iterate over the background images and take a screenshot of each
+        scroll(driver, html, img) # scroll to the image
+        save_screenshot(driver, save_path, res_name, " img-" + img.get_attribute("id") + " ")
+
+    driver.quit() # close the browser
+    
+
+def capture_mobile_screenshots(save_path):
+
+    mobile_resolutions = ['iPhone SE', 'iPhone XR', 'iPhone 12 Pro', 'iPhone 14 Pro Max', 'Pixel 7', 'Samsung Galaxy S8+']
+
+    for res in mobile_resolutions:
+        screenshot_mobile_res(res, save_path) # take a screenshot of the website with the mobile resolution
+
+def capture_screenshots():
     if os.path.isdir("generatedScreenshots") != True: # create a folder for the screenshots if it doesn't exist
         os.mkdir("generatedScreenshots")
 
@@ -74,14 +115,7 @@ def capture_screenshots(): # generates a screenshot of the start page in two res
     save_path = "generatedScreenshots/" + sub_folder_name + "/" # set the save path to the subfolder
     os.mkdir(save_path) # create the subfolder
 
-    screenshot_res(1920, 1080, "1080p", save_path) # test for checking desktop 1080p resolution
-    screenshot_res(2560, 1440, "1440p", save_path) # test for checking desktop 1440p resolution
-    # tests for checking phone resolutions
-    screenshot_res(375, 667, "iPhone-SE", save_path) # iPhone SE
-    screenshot_res(414, 896, "iPhone-XR", save_path) # iPhone XR
-    screenshot_res(390, 844, "iPhone-12-Pro", save_path) # iPhone 12 Pro
-    screenshot_res(430, 932, "iPhone-14-Pro-Max", save_path) # iPhone 14 Pro Max
-    screenshot_res(412, 915, "Pixel-7-Samsung-S20-Ultra", save_path) # Pixel 7 / Samsung Galaxy S20 Ultra
-    screenshot_res(360, 740, "Samsung-Galaxy-S8+", save_path) # Samsung Galaxy S8+
+    capture_desktop_screenshots(save_path)
+    capture_mobile_screenshots(save_path)
 
-capture_screenshots()
+capture_screenshots() # run the function to capture the screenshots
